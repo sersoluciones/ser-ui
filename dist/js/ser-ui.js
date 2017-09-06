845e0a4aa97872d68d9eb1249685ed245a1786e0
Andrés Gamba <xnovaerax@gmail.com>
2017-09-06 08:39:45 -0500

    Fix assets

--- dist/js/ser-ui.js
+++ dist/js/ser-ui.js
@@ -36120,7 +36120,7 @@ angular.module('SER', [
     // Actually loads the image
     SerImagePowered.src = url;
 
-})('https://www.sersoluciones.com/wp-content/uploads/2017/04/powered.jpg');
+})('https://s3.amazonaws.com/ser-ui/images/powered.jpg');
 
 
 //Routes & Http
