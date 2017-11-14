4e90a38f502b53ab4682dd39f1075080642f4f23
Carlos Andres Gamba Salamanca <andres_cgs@hotmail.com>
2017-11-14 02:51:41 -0500

    Add deleteAllCookies

--- src/js/sentry.js
+++ src/js/sentry.js
@@ -2,7 +2,7 @@ angular.module('SER.sentry', []);
 
 angular.module('SER.sentry').factory('$exceptionHandler', ['$window', '$log', function ($window, $log) {
 
-    if ($window.Raven && RAVEN_CONFIG_DSN) {
+    if ($window.Raven && RAVEN_CONFIG_DSN && !DEBUG) {
         console.log('Using the RavenJS exception handler.');
         Raven.config(RAVEN_CONFIG_DSN).install();
         return function (exception, cause) {
